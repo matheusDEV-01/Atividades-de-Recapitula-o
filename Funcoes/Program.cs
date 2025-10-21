75f041ceb42f3bdcbc4d9f8c224e486c1c8ee6e2
Matheus <matheusgg70jj@gmail.com>
2025-10-21 16:47:27 -0300

    Recaptulação das estruturas

---
+++ Funcoes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.IO.Compression;
+
+Console.Clear();
+Console.WriteLine("Digite seu nome");
+string nome = Console.ReadLine();
+Console.WriteLine("Digite seu sobrenome");
+string sobrenome = Console.ReadLine();
+
+Console.WriteLine($"ok, {nome}! Agora vamos dar as boas vindas a você!!");
+
+Console.WriteLine($"Por Favor escolha uma opção abaixo");
+Console.WriteLine($"    1 - Saudar com nome");
+Console.WriteLine($"    2 - Saudar com sobrenome");
+Console.WriteLine($"    3 - Saudar com nome e Sobrenome");
+Console.WriteLine($"    0 - Apenas Saudação Genérica");
+
+
+int opcao = 0;
+
+switch (opcao)
+{
+    case 0:
+        SaudacaoGenerica();
+        break;
+    case 1:
+        SaudarComNome(nome);
+        break;
+    case 2:
+        SaudarComSobrenome(sobrenome);
+        break;
+    case 3:
+        string nomeCompleto = DevolveNomeCompleto(nome, sobrenome);
+        Console.WriteLine($"Seja bem vindo, {nomeCompleto}");
+        break;
+
+
+}
+
+void SaudarComSobrenome(string sobrenomeRecebido)
+{
+    Console.WriteLine($"Olá, seja bem-vindo{sobrenomeRecebido}");
+}
+
+//Função que escreve uma saudação de forma genérica
+void SaudacaoGenerica()
+{
+    Console.WriteLine("Olá, seja bem vindo ao nosso programa, tenha um ótimo dia:");
+}
+
+//Recebe um nome e escreve uma saudação personalizada
+void SaudarComNome(string nomeRecebido)
+{
+    Console.WriteLine($"Seja bem vindo, {nomeRecebido}");
+}
+
+//recebe dois parametros e devolve um texto - recebe nome e sobrenome e devolve o nome completo
+string DevolveNomeCompleto(string nomeRecebido, string sobrenomeRecebido)
+{
+    return $"{nomeRecebido} {sobrenomeRecebido} ";
+}
---
+++ ReviewArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿//Arrays
+
+string nome = "Eduardo";
+Console.WriteLine($"Nome: {nome}");
+
+//cria um array tamalho 4 e preenche com vazio
+string[] nomes = new string[4];
+
+nomes[0] = "Maria";
+nomes[1] = "Matheus";
+nomes[2] = "Eloysa";
+nomes[3] = "Felipe";
+
+
+
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);
+Console.WriteLine(nomes[4]); //não existe
+
+Console.WriteLine();
+
+for(int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine(nomes[i]);
+}
